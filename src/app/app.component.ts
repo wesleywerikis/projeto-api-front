f609e28e9a4bbf560d9bc6baa0a6bfb207ecbc5d
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-10 16:15:44 -0300

    Add imports Form/HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PrincipalComponent } from "./principal/principal.component";
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, PrincipalComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'api-front';
-}
+   selector: 'app-root',
+   standalone: true,
+   imports: [CommonModule, RouterOutlet, PrincipalComponent, FormsModule, HttpClientModule],
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.css'
+  })
+  export class AppComponent {
+    title = 'api-front';
+  }
+
