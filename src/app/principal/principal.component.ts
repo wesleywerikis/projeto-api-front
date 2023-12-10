2cd5b9c16985be6cef402d40778af9ca03d777c6
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-10 19:04:36 -0300

    Methods and variables for requesting registered API clients

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Customer } from '../model/Customer';
+import { CustomerService } from '../service/customer.service';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './principal.component.html',
-  styleUrl: './principal.component.css'
+  styleUrl: './principal.component.css',
 })
 export class PrincipalComponent {
-   btnRegister:boolean = true;
+
+  customer = new Customer();
+
+  btnRegister: boolean = true;
+
+  customers: Customer[] = [];
+
+  constructor(private service: CustomerService) {}
+
+  select(): void {
+    this.service.select()
+    .subscribe(retorno => this.customers = retorno);
+  }
+
+  ngOnInit() {
+    this.select();
+  }
 }
