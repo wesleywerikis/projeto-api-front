3b71f843c33843d534ce65a89ef2d1c986931f6c
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-10 16:17:27 -0300

    Created variable btnRegister and imported CommonMod

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
-
+   btnRegister:boolean = true;
 }
